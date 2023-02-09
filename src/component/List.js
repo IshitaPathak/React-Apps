abe8a2bed20e3c2165ab0e797cea6abf4d3c87b4
IshitaPathak <pathakishita111@gmail.com>
2023-02-10 01:26:44 +0530

    app

---
+++ src/component/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function List(){
+    return(
+        <>
+        <ol>Top two web series
+            <li>Physics Wallah</li>
+            <li>Dark</li>
+        </ol>
+        </>
+    )
+}
+
+export default List;
